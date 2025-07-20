b1315eda04c68e7c1fc7d2be9afe7980d6ec0dbc
anzyanazzam <yunaakiraxd@gmail.com>
2025-07-20 16:30:18 +0700

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 'use client'
 import React from 'react'
 import Image from 'next/image'
-import { Swiper, SwiperSlide } from 'swiper/react'
+import { Swiper, SwiperSlide } from 'swiper'
 import 'swiper/css'
 
 type Props = {}
